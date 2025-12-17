3a6dc9f946edc86e5baaa5704043d1c45a9621ff
R3Nexe <nishhu24@gmail.com>
2025-12-17 22:11:59 +0530

    corrected heightchecker.py

--- day1_2/bonus questions/heightchecker.py
+++ day1_2/bonus questions/heightchecker.py
@@ -1,8 +1,10 @@
+class Solution(object):
+    def heightChecker(self,heights):
+                expected= sorted(heights)
+                count=0
+                for i in range(len(heights)):
+                                if heights[i]!=expected[i]:
+                                                count+=1
+                return count
 
-def containsDuplicate(nums):
-        myset=set()
-        for i in nums:
-                if i not in myset:
-                                myset.add()
-                else: return True
-print(containsDuplicate([2,14,18,22,22]))
+        
